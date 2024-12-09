f050b9531d5c1ef8fd90de8401a45d26ce15f078
shiv4j <119783592+shiv4j@users.noreply.github.com>
2024-12-09 10:10:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, Docker1!');
-});
-
-app.listen(3000, '0.0.0.0', () => {
-    console.log('Server is running on port 3000');
-});
